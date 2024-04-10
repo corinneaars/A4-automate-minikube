19f70c8d8830db1610669e6dda068a1cd97174b2
Rachel Koerner <rachel_koerner1@baylor.edu>
2024-04-09 21:29:23 -0500

    Name Change
    switched local directory filename for better cohesiveness

--- automate.sh
+++ automate.sh
@@ -2,7 +2,7 @@
 
 # Define repository URL and directory
 REPO_URL="https://github.com/corinneaars/A3-minikube-proj.git"
-LOCAL_DIR="A4-proj"  # Local directory where the repository will be cloned/pulled
+LOCAL_DIR="Assignment04-Production"  # Local directory where the repository will be cloned/pulled
 
 # Clone or pull the repository
 if [ -d "$LOCAL_DIR" ]; then
